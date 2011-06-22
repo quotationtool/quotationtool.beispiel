0061f77e2c2b7234475ac8ed95192da8610d83e7
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2011-06-22 15:40:39 +0200

    evolve user folder
    - evolve script to evolve user folder
    
    - added dependencies for z3c.table, z3c.batching and z3c.indexer

--- src/beispiel/generations/__init__.py
+++ src/beispiel/generations/__init__.py
@@ -14,6 +14,6 @@ from zope.app.generations.generations import SchemaManager
 
 BeispielSchemaManager = SchemaManager(
     minimum_generation = 5,
-    generation = 9,
+    generation = 10,
     package_name = 'beispiel.generations',
     )
