ff3469d66535737ac6cec71964e95de09de5d98c
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2011-07-02 17:46:17 +0200

    Overrinding widget label for example by id

---
+++ src/beispiel/browser/figuresng.py
@@ -0,0 +1,8 @@
+from z3c.form.widget import StaticWidgetAttribute
+
+from quotationtool.figuresng.browser.viewlet import example_id
+
+
+examplebyid_label = StaticWidgetAttribute(
+    u"ArB:",
+    context=None, request=None, view=None, field=example_id, widget=None)
